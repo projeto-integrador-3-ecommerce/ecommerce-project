4c8d6c29b2893956257c0d9b32b130ddd303c82b
Kami Vilela <vilelakamy18@gmail.com>
2026-09-20 22:54:00 -0300

    Updating

--- routes/orders.php
+++ routes/orders.php
@@ -5,7 +5,12 @@ use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/orders', [OrderController::class, 'store'])->name('orders.store');
+
 Route::get('/orders/{order}', [OrderController::class, 'show'])->name('orders.show');
+
 Route::delete('/orders/{order}', [OrderController::class, 'destroy'])->name('orders.destroy');
+
 Route::get('/orders/{order}/addresses', [AddressController::class, 'index'])->name('orders.addresses');
+
 Route::post('/orders/{order}/addresses', [OrderController::class, 'address'])->name('orders.address');
+
